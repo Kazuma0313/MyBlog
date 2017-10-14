ae314fe9c7a722f7f930cc70963672435f682fd8
kazuma <ka.zu.ma.h.wj@gmail.com>
2017-10-14 04:00:36 +0000

    added name

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 20171008132940) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
