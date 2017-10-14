8425653156dbc6644e672e6eeb21f418808b0064
kazuma <ka.zu.ma.h.wj@gmail.com>
2017-10-14 06:04:19 +0000

    Add validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171008132940) do
+ActiveRecord::Schema.define(version: 20171014032633) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
